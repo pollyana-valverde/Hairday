031e3f605260029c9a59418c9873174f862b9e89
Pollyana Valverde <valverdepolly@gmail.com>
2025-10-18 11:18:08 -0300

    Registrando um novo agendamento na API

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,5 +1,7 @@
 import dayjs from "dayjs";
 
+import {scheduleNew} from '../../services/schedule-new.js';
+
 const form = document.querySelector('form');
 const clientName = document.querySelector('#client')
 const selectedDate = document.querySelector('#date');
@@ -11,7 +13,7 @@ const inputToday = dayjs(new Date()).format('YYYY-MM-DD');
 selectedDate.value = inputToday;
 selectedDate.min = inputToday;
 
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
     // Evita o comportamento padrão do formulário
     event.preventDefault();
 
@@ -40,7 +42,7 @@ form.onsubmit = (event) => {
 
         const id = new Date().getTime()
 
-        console.log({
+        await scheduleNew({
             id,
             name,
             when,
--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -2,7 +2,7 @@ import {apiConfig} from './api-config';
 
 export async function scheduleNew({id, name, when}) {
     try {
-        await fetch(`${apiConfig}/schedules`, {
+        await fetch(`${apiConfig.baseURL}/schedules`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
